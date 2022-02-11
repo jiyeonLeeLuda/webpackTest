9653d30d2806cff171693bef52a0d604287e62b9
jiyeonLeeLuda <dlwldusq.work@gmail.com>
2022-02-11 15:19:17 +0900

    setup

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+//nodejs환경이다..
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  //파일 읽어들이기 시작하는 진입점 설정
+  entry: './js/main.js',
+
+  //결과물 번들을 반환하는 설정. path는 절대경로를 명시해줘야한다.
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'static',
+        },
+      ],
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+  },
+};
